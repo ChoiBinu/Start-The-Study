3488dc2d00fb62f3b14a7bb5016593daf8e4c32a
BINU <cmw9396@gmail.com>
2020-11-03 16:11:55 +0900

    블랙잭 미완성
    블랙잭 게임기능
    승리,패배,다시입력

---
+++ JS/test-2.js
@@ -0,0 +1,14 @@
+const NUMBER = 21;
+let no = prompt("Number", "숫자를 입력해주세요");
+    for(;;){
+        let sum = prompt("Number", "숫자를 입력해주세요") * 1;
+        no+=sum;
+
+        if(NUMBER === sum){
+            alert("GAME WIN");
+            break;
+        } else if (NUMBER <) {
+
+        }
+    }   
+    
